d03097e65068e9582b739d4de0cfe319a92e423b
Rohit Dashora <rohitdashora@gmail.com>
2021-01-31 22:53:29 +0530

    modified to split header and get core set

--- Try1.py
+++ Try1.py
@@ -1,8 +1,22 @@
 #import pandas
 import csv
+import re
 
-with open ("./data/a_example") as csv_file:
-    csv_reader = csv.reader(csv_file,delimiter = " ")
-    for row in csv_reader:
-        print(row)
+biglist=[]
+pizzas= []
 
+with open ("./data/a_example", 'r') as csv_file:
+    #data = csv_file.read().replace('\n', '')
+    header=csv_file.readline()
+    next(csv_file)
+    for row in csv_file:
+        row = row.rstrip("\n")
+        li = list(row.split(" ")) 
+        li.pop(0)
+        biglist= biglist+li
+        pizzas.append(li)
+print(biglist)
+print(pizzas)
+print(header)
+biglist = list(dict.fromkeys(biglist))
+print(biglist)
