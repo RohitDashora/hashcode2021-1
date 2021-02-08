5c6c806a6536307e38e542f6a06689faad729c17
Rohit Dashora <rohitdashora@gmail.com>
2021-02-08 22:33:20 +0530

    updated with tasks

--- Try1.py
+++ Try1.py
@@ -5,10 +5,24 @@ import re
 biglist=[]
 pizzas= []
 
+# Split the input file into two - Rohit
+# Find number of Unique ingredients - Manisha 
+    # We just need number not actual ingridentss
+    # use a data structure to hold this data (dataframe)
+# produce one file per team type with pizza this can be validated 
+# Combinations` -
+    # Compute all possible combinations where we can give each team closest number of ingridents to the max-
+        # geerate pizza ids (sort by max ingredients)
+        # List of Lists - with pop + a running counter + a dict with kes as team and vlaues as pizza ids 
+#scoring Module 
+    #find max ingridents per team 
+    # score
+    #produce ourput file
+
+
 with open ("./data/a_example", 'r') as csv_file:
-    #data = csv_file.read().replace('\n', '')
     header=csv_file.readline()
-    next(csv_file)
+    #next(csv_file)
     for row in csv_file:
         row = row.rstrip("\n")
         li = list(row.split(" ")) 
@@ -20,3 +34,7 @@ print(pizzas)
 print(header)
 biglist = list(dict.fromkeys(biglist))
 print(biglist)
+
+
+
+
