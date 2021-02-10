dd71806f2209253079f5062d74a532905f9df2af
Manisha Kemani <manishakemani91@gmail.com>
2021-02-10 21:06:11 +0530

    added logic to count unique values from array

--- Try1.py
+++ Try1.py
@@ -1,6 +1,8 @@
 #import pandas
 import csv
 import re
+import pandas as pd
+import numpy as nm
 
 biglist=[]
 pizzas= []
@@ -20,21 +22,32 @@ pizzas= []
     #produce ourput file
 
 
-with open ("./data/a_example", 'r') as csv_file:
-    header=csv_file.readline()
-    #next(csv_file)
-    for row in csv_file:
-        row = row.rstrip("\n")
-        li = list(row.split(" ")) 
-        li.pop(0)
-        biglist= biglist+li
-        pizzas.append(li)
-print(biglist)
-print(pizzas)
-print(header)
-biglist = list(dict.fromkeys(biglist))
-print(biglist)
+# with open ("./data/a_example", 'r') as csv_file:
+#     header=csv_file.readline()
+#     #next(csv_file)
+#     for row in csv_file:
+#         row = row.rstrip("\n")
+#         li = list(row.split(" ")) 
+#         li.pop(0)
+#         biglist= biglist+li
+#         pizzas.append(li)
+# print(biglist)
+# print(pizzas)
+# print(header)
+# biglist = list(dict.fromkeys(biglist))
+# print(biglist)
 
 
+#data = pd.read_csv("./data/a_example", sep=' ', index_col=0, keep_default_na=False)
+#its for file with less data
+data = pd.read_csv("./data/a_example", sep=' ', index_col=0,error_bad_lines=False)
 
+# print(data[1].value_counts())
+narray = nm.array(data)
 
+print(narray)
+
+df = pd.DataFrame(narray.ravel())
+
+#count of unique value
+print(df[0].nunique())
